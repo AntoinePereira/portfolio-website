3d740899e8a7bbbea5a17f7c55281e84586288b7
AntoinePereira <toine_42@hotmail.fr>
2021-03-17 09:41:43 +0100

    init netlify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TypingEffect/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import TypingEffect from '../components/TypingEffect.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TypingEffect
   }
 }
 </script>
